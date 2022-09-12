30ea24066137f825ae043a635afabe12e12adf23
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-12 14:22:39 +0600

    2nd commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const qrFormElement = document.getElementById("qr_form");
+const qrContainer = document.getElementById("qrContainer");
+const qrImage = document.getElementById("qrImage");
+const inputTextEL = document.getElementById("inputText");
+
+const renderQRCode = (url) =>{
+    if(!url) return;
+
+    qrImage.src = url;
+    qrContainer.classList.add("show");
+}
 
 qrFormElement.addEventListener("submit",(event)=>{
     event.preventDefault();
@@ -8,7 +18,16 @@ qrFormElement.addEventListener("submit",(event)=>{
 
     const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`
 
-    console.log(text)
+    // console.log(text)
 
+    renderQRCode(qrCodeUrl);
+
+})
+
+inputTextEL.addEventListener("Keyup",()=>{
+    
+    if(!inputTextEL.value.trim()){
+        qrContainer.classList.remove("show")
+    }
 
 })
